acbaa889b5735bccb2653f7abd6b30ff8748a181
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-10-15 11:21:06 +0100

    Updated scary virus

--- virus.bat
+++ virus.bat
@@ -2,4 +2,16 @@
 color 2
 :start
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 1
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 4
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 1
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 5
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 3
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 4
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 goto start
