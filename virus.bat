3828ef522e87f88eb275d7e5a4ade31cd381519f
JayRowlands <56541131+JayRowlands@users.noreply.github.com>
2021-10-15 11:25:59 +0100

    Super updated scary virus, dont open!!!

--- virus.bat
+++ virus.bat
@@ -1,17 +1,17 @@
 @echo off
+FOR /L %%A IN (1,1,10) DO (
 color 2
 :start
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
-color 1
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 color 4
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 color 1
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 color 5
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 color 3
-echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 color 4
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+)
+color 2
+echo "You are being hacked....."
+TIMEOUT 5
 goto start
